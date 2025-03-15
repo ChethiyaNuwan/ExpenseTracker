8063ead1defb36fb24bd95a6b4a9f3c975a8f3f5
Chethiya Nuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2025-03-15 14:38:58 +0530

    added maui template

---
+++ Native/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Native;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Native/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Native;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
